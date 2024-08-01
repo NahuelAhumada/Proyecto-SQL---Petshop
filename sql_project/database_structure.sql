782121f27a0e1d0ad943fb1a3154347072b634b2
NahuelAhumada <ahumada.nahuel35149@gmail.com>
2024-08-01 14:28:23 -0300

    Cambio en la estrucutar - on delete/update y tabla intermedia USUARIO_DIRECCION

--- sql_project/database_structure.sql
+++ sql_project/database_structure.sql
@@ -31,13 +31,12 @@ CREATE TABLE USUARIOS_DIRECCIONES(
 CREATE TABLE CARRITOS (
 	id_carrito int PRIMARY KEY AUTO_INCREMENT,
     id_usuario int UNIQUE NOT NULL,
-    fecha_creacion datetime NOT NULL DEFAULT(now()),
-    fecha_ultima_actualizacion datetime NOT NULL DEFAULT(now()),
-    total_a_pagar float(10, 2)
+    fecha_interaccion datetime NOT NULL DEFAULT(now())
 );
 CREATE TABLE ITEM_CARRITO (
     id_carrito int NOT NULL,
     id_producto int NOT NULL,
+    precio_final decimal(10, 2),
     cantidad int NOT NULL,
     PRIMARY KEY (id_carrito, id_producto)
 );
@@ -46,14 +45,13 @@ CREATE TABLE ORDENES_DE_COMPRA (
 	id_orden int PRIMARY KEY AUTO_INCREMENT,
     id_usuario int NOT NULL,
     id_metodo_pago int NOT NULL,
-    fecha_de_orden datetime NOT NULL DEFAULT(now()),
-    total_a_pagar float(10, 2)
+    fecha_de_orden datetime NOT NULL DEFAULT(now())
 );
 
 CREATE TABLE DETALLE_DE_ORDEN (
 	id_orden int NOT NULL,
 	id_producto int NOT NULL,
-    precio_final float(10, 2),
+    precio_final decimal(10, 2),
 	cantidad int NOT NULL,
     PRIMARY KEY (id_orden, id_producto)
 );
@@ -119,9 +117,12 @@ ALTER TABLE PRODUCTOS ADD FOREIGN KEY (id_subcategoria) REFERENCES SUBCATEGORIAS
 
 ALTER TABLE PRODUCTOS ADD FOREIGN KEY (id_marca) REFERENCES MARCAS (id_marca);
 
-ALTER TABLE SUBCATEGORIAS ADD FOREIGN KEY (id_categoria) REFERENCES CATEGORIAS (id_categoria);
+ALTER TABLE SUBCATEGORIAS ADD FOREIGN KEY (id_categoria) REFERENCES CATEGORIAS (id_categoria)
+ON UPDATE CASCADE;
 
-ALTER TABLE CATEGORIAS ADD FOREIGN KEY (id_animal) REFERENCES ANIMALES (id_animal);
+ALTER TABLE CATEGORIAS ADD FOREIGN KEY (id_animal) REFERENCES ANIMALES (id_animal)
+ON UPDATE CASCADE;
+
 
 ALTER TABLE DETALLE_DE_ORDEN ADD FOREIGN KEY (id_orden) REFERENCES ORDENES_DE_COMPRA (id_orden);
 
@@ -130,4 +131,3 @@ ALTER TABLE DETALLE_DE_ORDEN ADD FOREIGN KEY (id_producto) REFERENCES PRODUCTOS 
 ALTER TABLE ORDENES_DE_COMPRA ADD FOREIGN KEY (id_usuario) REFERENCES USUARIOS (id_usuario);
 
 ALTER TABLE ORDENES_DE_COMPRA ADD FOREIGN KEY (id_metodo_pago) REFERENCES METODOS_DE_PAGO (id_metodo_pago);
-
