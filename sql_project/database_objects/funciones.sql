4419a4c821759c44288f481b775d242deb73d95d
NahuelAhumada <ahumada.nahuel35149@gmail.com>
2024-08-07 23:12:50 -0300

    Funciones agregagadas de precio total y ultimo id_orden de usuario

--- sql_project/database_objects/funciones.sql
+++ sql_project/database_objects/funciones.sql
@@ -1,37 +1,54 @@
+USE petshop_ecommerce;
+DROP FUNCTION IF EXISTS id_ultima_orden_de_compra_de_usuario;
 DELIMITER //
-
-CREATE FUNCTION UltimaFechaDeCompra(var_id_usuario INT) RETURNS DATETIME
+CREATE FUNCTION id_ultima_orden_de_compra_de_usuario(var_id_usuario INT) RETURNS INT
 DETERMINISTIC
 READS SQL DATA
 BEGIN
-	DECLARE ultima_fecha DATETIME;
-    SELECT MAX(fecha_de_orden)
-    INTO ultima_fecha
-    FROM ordenes_de_compra
-    WHERE id_usuario = var_id_usuario;
+	DECLARE ultimo_id INT;
     
-    RETURN ultima_fecha;
+    SELECT id_orden
+    INTO ultimo_id
+    FROM ORDENES_DE_COMPRA
+	WHERE id_usuario =var_id_usuario
+    AND fecha_de_orden = (
+		SELECT MAX(fecha_de_orden)
+        FROM ORDENES_DE_COMPRA
+        WHERE id_usuario = var_id_usuario);
+    RETURN ultimo_id;
 END //
 DELIMITER ;
 
-
-
-DROP FUNCTION IF EXISTS CalcularPrecioTotalDeOrden;
+DROP FUNCTION IF EXISTS calcular_precio_total_de_orden;
 DELIMITER //
-CREATE FUNCTION CalcularPrecioTotalDeOrden(var_id_orden INT) RETURNS FLOAT
+CREATE FUNCTION calcular_precio_total_de_orden(var_id_orden INT) RETURNS VARCHAR(14)
 DETERMINISTIC
 READS SQL DATA
 BEGIN
-	DECLARE resultado FLOAT;
+	DECLARE resultado_operacion DECIMAL(10,2);
+    DECLARE resultado_concatenado VARCHAR(14);
     SELECT SUM(precio_final * cantidad) 
-    INTO resultado
+    INTO resultado_operacion
     FROM DETALLE_DE_ORDEN
     WHERE id_orden = var_id_orden;
-    
-    RETURN RESULTADO;
+	SET resultado_concatenado = CONCAT("$",CAST(resultado_operacion AS CHAR(13)));
+    RETURN resultado_concatenado;
 END //
 DELIMITER ;
 
-SELECT id_orden, CalcularPrecioTotalDeOrden(id_orden) AS 'Total a pagar'
-FROM detalle_de_orden
-GROUP BY id_orden;
+DROP FUNCTION IF EXISTS calcular_precio_total_de_carrito;
+DELIMITER //
+CREATE FUNCTION calcular_precio_total_de_carrito(var_id_carrito INT) RETURNS VARCHAR(14)
+DETERMINISTIC
+READS SQL DATA
+BEGIN
+	DECLARE resultado_operacion DECIMAL(10,2);
+    DECLARE resultado_concatenado VARCHAR(14);
+    SELECT SUM(precio_final * cantidad) 
+    INTO resultado_operacion
+    FROM ITEM_CARRITO
+    WHERE id_carrito = var_id_carrito;
+	SET resultado_concatenado = CONCAT("$",CAST(resultado_operacion AS CHAR(13)));
+    RETURN resultado_concatenado;
+END //
+DELIMITER ;
