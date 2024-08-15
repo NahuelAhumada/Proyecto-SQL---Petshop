81ea0875c8f71e3a3775c653fb42e8138ecf87ea
NahuelAhumada <ahumada.nahuel35149@gmail.com>
2024-08-14 22:55:28 -0300

    Refactorizacion de funciones

--- sql_project/database_objects/funciones.sql
+++ sql_project/database_objects/funciones.sql
@@ -1,4 +1,19 @@
 USE petshop_ecommerce;
+
+
+/*# Funcion para visualizar precio como un varchar, anteponiendo el caracter $*/
+DROP FUNCTION IF EXISTS pasar_precio_a_varchar;
+
+DELIMITER //
+CREATE FUNCTION pasar_precio_a_varchar(var_precio DECIMAL(15,2)) RETURNS VARCHAR(18)
+DETERMINISTIC
+NO SQL
+BEGIN
+	RETURN CONCAT("$",CAST(var_precio AS CHAR(17)));
+END //
+DELIMITER ;
+
+
 DROP FUNCTION IF EXISTS id_ultima_orden_de_compra_de_usuario;
 DELIMITER //
 CREATE FUNCTION id_ultima_orden_de_compra_de_usuario(var_id_usuario INT) RETURNS INT
@@ -6,30 +21,28 @@ DETERMINISTIC
 READS SQL DATA
 BEGIN
 	DECLARE ultimo_id INT;
-    
-    SELECT id_orden
+    SELECT max.id_orden
     INTO ultimo_id
-    FROM ORDENES_DE_COMPRA
-	WHERE id_usuario =var_id_usuario
-    AND fecha_de_orden = (
-		SELECT MAX(fecha_de_orden)
-        FROM ORDENES_DE_COMPRA
-        WHERE id_usuario = var_id_usuario);
-	IF ultimo_id IS NULL THEN
-		SIGNAL SQLSTATE '02000' SET MESSAGE_TEXT = 'No hay orden asociada a este usuario', MYSQL_ERRNO = 1000;
-    END IF;
+    FROM (SELECT id_orden, MAX(fecha_de_orden) as ultima_fecha
+		FROM ORDENES_DE_COMPRA
+		WHERE id_usuario = var_id_usuario
+        GROUP BY id_orden
+        ORDER BY ultima_fecha DESC
+        LIMIT 1) as max;
+
     RETURN ultimo_id;
 END //
 DELIMITER ;
 
+
 DROP FUNCTION IF EXISTS calcular_precio_total_de_orden;
 DELIMITER //
-CREATE FUNCTION calcular_precio_total_de_orden(var_id_orden INT) RETURNS VARCHAR(14)
+CREATE FUNCTION calcular_precio_total_de_orden(var_id_orden INT) RETURNS VARCHAR(18)
 DETERMINISTIC
 READS SQL DATA
 BEGIN
-	DECLARE resultado_operacion DECIMAL(10,2);
-    DECLARE resultado_concatenado VARCHAR(14);
+	DECLARE resultado_operacion DECIMAL(15,2);
+    DECLARE resultado_concatenado VARCHAR(18);
     SELECT SUM(precio_final * cantidad) 
     INTO resultado_operacion
     FROM DETALLE_DE_ORDEN
@@ -37,19 +50,19 @@ BEGIN
     IF resultado_operacion IS NULL THEN
 		SIGNAL SQLSTATE '02000' SET MESSAGE_TEXT = 'ID de ORDEN invalida', MYSQL_ERRNO = 1000;
     END IF;
-	SET resultado_concatenado = CONCAT("$",CAST(resultado_operacion AS CHAR(13)));
+	SET resultado_concatenado = pasar_precio_a_varchar(resultado_operacion);
     RETURN resultado_concatenado;
 END //
 DELIMITER ;
 
 DROP FUNCTION IF EXISTS calcular_precio_total_de_carrito;
 DELIMITER //
-CREATE FUNCTION calcular_precio_total_de_carrito(var_id_carrito INT) RETURNS VARCHAR(14)
+CREATE FUNCTION calcular_precio_total_de_carrito(var_id_carrito INT) RETURNS VARCHAR(18)
 DETERMINISTIC
 READS SQL DATA
 BEGIN
-	DECLARE resultado_operacion DECIMAL(10,2);
-    DECLARE resultado_concatenado VARCHAR(14);
+	DECLARE resultado_operacion DECIMAL(15,2);
+    DECLARE resultado_concatenado VARCHAR(18);
     SELECT SUM(precio_final * cantidad) 
     INTO resultado_operacion
     FROM ITEM_CARRITO
@@ -57,7 +70,7 @@ BEGIN
     IF resultado_operacion IS NULL THEN
 		SIGNAL SQLSTATE '02000' SET MESSAGE_TEXT = 'No hay productos para este carrito', MYSQL_ERRNO = 1000;
     END IF;
-	SET resultado_concatenado = CONCAT("$",CAST(resultado_operacion AS CHAR(13)));
+	SET resultado_concatenado = pasar_precio_a_varchar(resultado_operacion);
     RETURN resultado_concatenado;
 END //
 DELIMITER ;
