383e4b497565475390fcbac9d9773b2b2b2043f3
NahuelAhumada <ahumada.nahuel35149@gmail.com>
2024-08-15 11:22:58 -0300

    Se agrego una funcion para calcular precio de acuerdo al metodo de pago

--- sql_project/database_objects/funciones.sql
+++ sql_project/database_objects/funciones.sql
@@ -1,7 +1,7 @@
 USE petshop_ecommerce;
 
 
-/*# Funcion para visualizar precio como un varchar, anteponiendo el caracter $*/
+# Funcion para visualizar precio como un varchar, anteponiendo el caracter $
 DROP FUNCTION IF EXISTS pasar_precio_a_varchar;
 
 DELIMITER //
@@ -10,6 +10,27 @@ DETERMINISTIC
 NO SQL
 BEGIN
 	RETURN CONCAT("$",CAST(var_precio AS CHAR(17)));
+END //
+DELIMITER ;
+
+# Funcion para calcular precio, de acuerdo a un determinado metodo de pago
+DROP FUNCTION IF EXISTS calcular_precio_final;
+
+DELIMITER //
+CREATE FUNCTION calcular_precio_final(var_precio DECIMAL(15,2), var_id_metodo_de_pago INT) RETURNS DECIMAL(15,2)
+DETERMINISTIC
+NO SQL
+BEGIN
+	DECLARE precio_resultado DECIMAL (15,2);
+    SET precio_resultado = var_precio;
+	#Descuento 10% por pagar en efectivo
+	IF var_id_metodo_de_pago = 1 THEN
+		 SET precio_resultado = precio_resultado * 0.9;
+	#Aumento de 20% por pagar en 3 cuotas
+    ELSEIF var_id_metodo_de_pago = 5 THEN
+		SET precio_resultado = precio_resultado * 1.2;
+	END IF;
+	RETURN precio_resultado;
 END //
 DELIMITER ;
 
@@ -63,10 +84,13 @@ READS SQL DATA
 BEGIN
 	DECLARE resultado_operacion DECIMAL(15,2);
     DECLARE resultado_concatenado VARCHAR(18);
-    SELECT SUM(precio_final * cantidad) 
+    SELECT SUM(p.precio_final * i.cantidad) 
     INTO resultado_operacion
-    FROM ITEM_CARRITO
-    WHERE id_carrito = var_id_carrito;
+    FROM ITEM_CARRITO as i
+    JOIN  PRODUCTOS as p
+    ON (i.id_producto = p.id_producto
+    AND id_carrito = var_id_carrito);
+    
     IF resultado_operacion IS NULL THEN
 		SIGNAL SQLSTATE '02000' SET MESSAGE_TEXT = 'No hay productos para este carrito', MYSQL_ERRNO = 1000;
     END IF;
