b9b69c87dcbe624b8a0a5a5beb9313ec7a659734
NahuelAhumada <ahumada.nahuel35149@gmail.com>
2024-08-17 16:20:55 -0300

    Renombrado de vistas, refactorizacion productos_vendidos y agregado de total a pagar por orden de compra

--- sql_project/database_objects/vistas.sql
+++ sql_project/database_objects/vistas.sql
@@ -1,6 +1,5 @@
 USE petshop_ecommerce;
-CREATE OR REPLACE VIEW 
-	TotalAPagarPorCarrito AS 
+CREATE OR REPLACE VIEW PRECIO_TOTAL_DE_CARRITOS AS 
 		SELECT
 			us.id_usuario,
 			us.nombre_de_usuario,
@@ -9,49 +8,53 @@ CREATE OR REPLACE VIEW
         INNER JOIN USUARIOS as us ON (us.id_usuario=cart.id_usuario)
         GROUP BY us.id_usuario, us.nombre_de_usuario;
 
-CREATE OR REPLACE VIEW
-	CantidadCompradaDeProductos AS
-		SELECT calculo.id_producto, p.nombre,  calculo.Cantidad_Comprada
-        FROM (SELECT id_producto, SUM(cantidad) AS Total_Comprado
-			FROM DETALLE_DE_ORDEN
-			GROUP BY id_producto) as calculo
-		JOIN PRODUCTOS as p
-        ON (calculo.id_producto = p.id_producto);
+CREATE OR REPLACE VIEW TOTAL_A_PAGAR_POR_ORDEN_DE_COMPRA AS
+    SELECT
+		id_orden,
+        estado,
+        fecha_de_orden,
+        calcular_precio_total_de_orden(id_orden) as monto_estandar
+	FROM ORDENES_DE_COMPRA;
+    
+CREATE VIEW VISTA_PRODUCTOS_POR_TOTAL_VENDIDOS AS
+SELECT 
+    p.id_producto,
+    p.nombre,
+    SUM(detalles.cantidad) AS total_vendido
+FROM 
+    PRODUCTOS p
+JOIN 
+    DETALLE_DE_ORDEN do ON p.id_producto = detalles.id_producto
+GROUP BY 
+    p.id_producto, p.nombre;
 
 CREATE OR REPLACE VIEW 
-	ProductosVendidoPorPrecioFinal AS
+	HISTORIAL_VENTA_DE_PRODUCTOS_POR_MONTO_FINAL AS
 	SELECT
-	o.id_orden,
-    o.fecha_de_orden,
-    p.id_producto,
-    p.nombre as nombre_de_producto,
-    CONCAT('$ ', CAST(d.precio_final AS CHAR(13))) AS 'Precio de venta',
-    d.cantidad
+		o.id_orden,
+		o.fecha_de_orden,
+		p.id_producto,
+		p.nombre as nombre_de_producto,
+		mostrar_precio(d.precio_final) AS precio_de_venta,
+		d.cantidad
     FROM
     PRODUCTOS as P
     JOIN DETALLE_DE_ORDEN AS d ON (p.id_producto = d.id_producto)
 	JOIN ORDENES_DE_COMPRA AS o ON(d.id_orden = o.id_orden);
 
 CREATE OR REPLACE VIEW 
-	FechaUltimaCompraDeCadaUsuario AS
-		SELECT u.id_usuario, u.nombre_de_usuario, u.email, MAX(o.fecha_de_orden) as 'Ultima fecha de compra' 
-		FROM USUARIOS AS u
-		JOIN ORDENES_DE_COMPRA AS o ON(u.id_usuario=o.id_usuario)
-		GROUP BY  u.id_usuario, u.nombre_de_usuario, u.email;
-    
-CREATE OR REPLACE VIEW 
-	VisualizacionDeSubcategorias AS
-		SELECT sub.id_subcategoria, sub.nombre as 'Subcategoria', cat.nombre as 'Categoria', a.nombre as 'Animal'
-		FROM SUBCATEGORIAS as sub
-		JOIN CATEGORIAS as cat ON (sub.id_categoria = cat.id_categoria)
-		JOIN ANIMALES as a ON (cat.id_animal = a.id_animal);
+	VISUALIZACION_DE_SUBCATEGORIAS AS
+	SELECT sub.id_subcategoria, sub.nombre as 'Subcategoria', cat.nombre as 'Categoria', a.nombre as 'Animal'
+	FROM SUBCATEGORIAS as sub
+	JOIN CATEGORIAS as cat ON (sub.id_categoria = cat.id_categoria)
+	JOIN ANIMALES as a ON (cat.id_animal = a.id_animal);
 
 CREATE OR REPLACE VIEW  
-	UsuariosQueNoCompraronEn3Meses AS
-		SELECT u.id_usuarios, u.nombre_de_usuario, u.email
-		FROM USUARIOS AS u
-		WHERE u.id_usuario NOT IN(
-			SELECT o.id_usuario
-			FROM ORDENES_DE_COMPRA
-			WHERE fecha_de_orden >= DATE_SUB(NOW(), INTERVAL 3 MONTH)
+	UUSUARIOS_SIN_COMPRAS_HACE_3_MESES AS
+	SELECT u.id_usuarios, u.nombre_de_usuario, u.email
+	FROM USUARIOS AS u
+	WHERE u.id_usuario NOT IN(
+		SELECT DISTINCT o.id_usuario
+		FROM ORDENES_DE_COMPRA as o
+		WHERE fecha_de_orden >= DATE_SUB(NOW(), INTERVAL 3 MONTH)
     );
